6916c1759878126b071a8745ed78b02cad9eec9d
fedecodelab <federicoguzman.css@gmail.com>
2024-09-25 14:16:59 -0300

    arreglando tamaños

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-6">
+    <div className="flex flex-col justify-center items-center h-screen gap-6 w-[90%] mx-auto">
       <h1 className="text-3xl font-semibold text-zinc-700">
-        Error 404 - Página no encontrada
+        Error 404 - Page Not Found
       </h1>
       <img src="images/robot.png" alt="" className="object-cover h-80" />
     </div>
