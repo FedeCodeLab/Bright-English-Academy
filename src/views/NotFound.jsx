04286204097a0ab0acbfbd93baf2e5d716acef1f
fedecodelab <federicoguzman.css@gmail.com>
2024-09-25 14:12:11 -0300

    cambio en el texto

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-6">
       <h1 className="text-3xl font-semibold text-zinc-700">
-        Página no encontrada
+        Error 404 - Página no encontrada
       </h1>
       <img src="images/robot.png" alt="" className="object-cover h-80" />
     </div>
