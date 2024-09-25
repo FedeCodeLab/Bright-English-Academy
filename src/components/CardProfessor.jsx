43e7101b0b67b48228b14c8fcfb180c4570d6bdb
fedecodelab <federicoguzman.css@gmail.com>
2024-09-25 12:23:21 -0300

    Section profesores terminada

--- src/components/CardProfessor.jsx
+++ src/components/CardProfessor.jsx
@@ -1,9 +1,13 @@
 export default function CardProfessor({ title, description, image }) {
   return (
-    <div className="bg-indigo-800 rounded-lg px-6 py-8 shadow-lg text-white transform hover:scale-105 select-none cursor-pointer">
+    <div className="flex flex-col items-center gap-3 text-center bg-white rounded-lg px-6 py-8 shadow-lg">
+      <img
+        src={image}
+        alt={title}
+        className="w-[50%] lg:w-[80%] aspect-square rounded-full object-cover"
+      />
       <p className="text-xl font-semibold ">{title}</p>
       <p className=" pb-6">{description}</p>
-      <img src={image} alt={title} />
     </div>
   );
 }
--- src/data/professorsData.js
+++ src/data/professorsData.js
@@ -3,49 +3,57 @@ const professorsData = [
     title: "María López",
     description:
       "Profesora con más de 10 años de experiencia en la enseñanza de inglés para principiantes. Su enfoque se centra en crear un ambiente de aprendizaje amigable y efectivo, utilizando juegos y actividades interactivas que fomentan la participación activa de los estudiantes.",
-    image: "url_de_la_imagen_1.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/09/19/10/37/ai-generated-9058407_1280.jpg",
   },
   {
     title: "Carlos Martínez",
     description:
       "Experto en inglés avanzado y preparación para exámenes como el IELTS y TOEFL. Con más de 8 años de experiencia, Carlos utiliza métodos innovadores y personalizados para ayudar a sus estudiantes a alcanzar sus metas lingüísticas y a superar los desafíos de los exámenes.",
-    image: "url_de_la_imagen_2.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/09/19/10/37/ai-generated-9058410_1280.jpg",
   },
   {
     title: "Lucía García",
     description:
       "Instructora apasionada por la enseñanza de habilidades de conversación y pronunciación. Con una metodología centrada en la práctica oral, Lucía ayuda a sus estudiantes a ganar confianza en el uso del inglés en situaciones cotidianas y profesionales.",
-    image: "url_de_la_imagen_3.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/09/19/10/37/ai-generated-9058409_1280.jpg",
   },
   {
     title: "Gema Fractal",
     description:
       "Profesora especializada en técnicas de aprendizaje acelerado y metodologías de enseñanza. Gema tiene un enfoque único que combina elementos visuales y auditivos para facilitar la comprensión y retención del contenido, adaptándose a las necesidades de cada estudiante.",
-    image: "url_de_la_imagen_4.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/08/20/14/26/ai-generated-8983488_1280.jpg",
   },
   {
     title: "Diego Ramírez",
     description:
       "Creador de materiales didácticos innovadores y recursos interactivos. Diego se enfoca en la gamificación del aprendizaje, ofreciendo a sus estudiantes un enfoque divertido y dinámico que les permite aprender inglés de manera efectiva y amena.",
-    image: "url_de_la_imagen_5.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/09/12/21/20/ai-generated-9043363_1280.png",
   },
   {
     title: "Sofía Castillo",
     description:
       "Focalizada en el inglés para negocios y profesionales. Sofía cuenta con una vasta experiencia en el ámbito corporativo, lo que le permite ofrecer un programa específico para aquellos que desean mejorar su comunicación en el entorno laboral y avanzar en sus carreras.",
-    image: "url_de_la_imagen_6.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/05/23/20/18/ai-generated-8783789_1280.jpg",
   },
   {
     title: "Fernando López",
     description:
       "Facilita el aprendizaje a través de dinámicas interactivas y un enfoque práctico. Con su estilo enérgico y motivador, Fernando crea un ambiente en el que los estudiantes se sienten cómodos para expresarse y experimentar con el idioma.",
-    image: "url_de_la_imagen_7.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/08/22/05/50/handsome-8988318_1280.jpg",
   },
   {
     title: "Ana Beltrán",
     description:
       "Experta en la enseñanza de inglés a niños y adolescentes. Ana utiliza métodos creativos y adaptados a diferentes edades, incorporando cuentos, juegos y actividades que estimulan el interés y la curiosidad de los más jóvenes por el aprendizaje del idioma.",
-    image: "url_de_la_imagen_8.jpg",
+    image:
+      "https://cdn.pixabay.com/photo/2024/08/31/05/47/ai-generated-9010549_1280.png",
   },
 ];
 
