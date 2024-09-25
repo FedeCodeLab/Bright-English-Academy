8677ba04441aab1eac5af66cb24d74644767c840
fedecodelab <federicoguzman.css@gmail.com>
2024-09-25 17:53:40 -0300

    menu hamburguesa

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Hamburguesa from "../icons/hamburguesa";
+import Hamburguesa from "../icons/Hamburguesa";
 import { Link } from "react-router-dom";
 import {
   Modal,
