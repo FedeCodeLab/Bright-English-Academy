6657d29bd0779df90f2fbe4edbcc1cf073fcc944
fedecodelab <federicoguzman.css@gmail.com>
2024-09-25 17:42:13 -0300

    arreglo

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -78,7 +78,7 @@ export default function Header() {
 
           <nav className="hidden md:flex gap-3 font-medium text-violet-700 items-center">
             <Link to="/">Inicio</Link>
-            <Link to="/about">Nosotros</Link>
+            <Link to="/about">Sobre nosotros</Link>
             <Link to="/professors">Profesores</Link>
             <Button
               className="text-white bg-violet-700 py-2 px-4 rounded-lg"
